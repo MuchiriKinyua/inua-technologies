4d0a0b7c408344fb089f8730bf32cc7dadea14f7
muchirikinyua <muchirikinyua2000@gmail.com>
2024-04-27 13:33:40 +0300

    removing register and login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,7 +24,7 @@
         <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
             <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
-                    <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
+                    <!-- <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
                         
                         @if (Route::has('login'))
                             <nav class="-mx-3 flex flex-1 justify-end">
@@ -54,7 +54,7 @@
                                 @endauth
                             </nav>
                         @endif
-                    </header>
+                    </header> -->
 
                     <main class="mt-6">
                 <div id="header" class="move-left">
